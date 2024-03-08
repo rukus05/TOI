16ff83037961bb7d79862a6de5dd1c701331b678
vboxuser <zkasilag@gmail.com>
2024-03-08 14:49:46 -0800

    2024 03 08 update

---
+++ PTO/test.py
@@ -0,0 +1,8 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+#print(os.listdir('..'))
+
+root_dir = os.path.abspath('..')
+
+print(root_dir)
--- locations.py
+++ locations.py
@@ -1,97 +1,3 @@
-
-class RemoveSpacesDict(dict):
-    
-    # Constructor Method
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    # Helper method that returns the stripped key
-    def _strip_key(self, key):
-        return key.strip()
-    
-    # Helper method that returns 2 keys; original and stripped key, which could be the same.
-    def _generate_lookup_keys(self, key):
-        return key, self._strip_key(key)
-    
-    # Method that overrides original Dict key retrieval method.
-    # Calls _generate_lookup_keys to get 2 kesy.
-    # Uses only the stripped key for the dictionary lookup.
-    #  This allows you to look up keys with or without trailing spaces at the end, and it retrieves the value associated with the stripped key.
-    def __getitem__(self, key):
-        original_key, stripped_key = self._generate_lookup_keys(key)
-        return super().__getitem__(stripped_key)
-
-    # Method that overrides original Dict key assignment method.  Similar to above method
-    # Only stripped key is used for assigning the key value.  
-    def __setitem__(self, key, value):
-        original_key, stripped_key = self._generate_lookup_keys(key)
-        super().__setitem__(stripped_key, value)
-
-
-
-
-# Department : GL Account 
-pto_gl_dict = {
-    100100 : 52500, \
-    100200 : 52500, \
-    100300 : 52500, \
-    100400 : 60400, \
-    200100 : 60400, \
-    200200 : 60400, \
-    300100 : 60400, \
-    300200 : 60400, \
-    300250 : 60400, \
-    300300 : 60400, \
-    300400 : 60400, \
-    300500 : 60400, \
-    400100 : 52500, \
-    700100 : 60400, \
-    700110 : 60400, \
-    700120 : 60400, \
-    700200 : 60400, \
-    700205 : 60400, \
-    700210 : 60400, \
-    700215 : 60400, \
-    700230 : 60400, \
-    700300 : 60400, \
-    700305 : 60400, \
-    700310 : 60400, \
-    700315 : 60400, \
-    700320 : 60400, \
-    700325 : 60400, \
-    700326 : 60400, \
-    700327 : 60400, \
-    700330 : 60400, \
-    700335 : 60400, \
-    700340 : 60400, \
-    700345 : 60400, \
-    700350 : 60400, \
-    700355 : 60400, \
-    700360 : 60400, \
-    701000 : 60400, \
-    701100 : 60400, \
-    701200 : 60400, \
-    701210 : 60400, \
-    702010 : 60400, \
-    702100 : 60400, \
-    702300 : 60400, \
-    702400 : 60400, \
-    702410 : 60400, \
-    702415 : 60400, \
-    702420 : 60400, \
-    703000 : 60400, \
-    704000 : 60400, \
-    704100 : 60400, \
-    704500 : 60400, \
-    704505 : 60400, \
-    705000 : 60400, \
-    705500 : 60400, \
-    705600 : 60400
-}
-
-# Create an instance of the custom RemoveSpacesDict class, which will contain the dictionary of locations.
-locations_dict = RemoveSpacesDict()
-
 locations_dict = {
     'The Oncology Institute, Inc.' : 101, \
     'Parent' : 100, \
@@ -208,7 +114,7 @@ locations_dict = {
     'TOI Marana CLOSED' : 1039, \
     'TOI Chino/Chino Hills' : 1055, \
     'TOI Hollywood' : 2014, \
-    'TOI R1-Downey RadOnc' : 1802, \
+    'TOI R1-Downey RadOnce' : 1802, \
     'TOI R-1 Pomona RadOnc' : 1803, \
     'TOI R1-Covina RadOnc' : 1804, \
     'TOI R1-Victorville RadOnc' : 1805, \
